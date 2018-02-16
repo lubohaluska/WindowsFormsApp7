1780ece627f68ff37358aeefaa1c4d4d1b2958dc
CSharpFri <CSharpFri@CLASS-2-06>
2018-02-16 17:58:00 +0100

    Add project files.

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace WindowsFormsApp7
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bSend = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bSend
+            // 
+            this.bSend.Location = new System.Drawing.Point(502, 192);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(75, 23);
+            this.bSend.TabIndex = 0;
+            this.bSend.Text = "Send";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tbMessage.Location = new System.Drawing.Point(12, 192);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(484, 20);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // tbChat
+            // 
+            this.tbChat.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tbChat.Location = new System.Drawing.Point(12, 12);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.ReadOnly = true;
+            this.tbChat.Size = new System.Drawing.Size(484, 174);
+            this.tbChat.TabIndex = 2;
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 227);
+            this.Controls.Add(this.tbChat);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.bSend);
+            this.Name = "MyForm";
+            this.Text = "MyForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+            string s = tbMessage.Text;
+            tbChat.Text += s + Environment.NewLine;
+
+            tbMessage.Text = "";
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.TextBox tbChat;
+    }
+}
+
